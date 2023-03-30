340a70e344e33eb5552331301beac7bd988089a6
Nice3w <129052474+Nice3w@users.noreply.github.com>
2023-03-30 10:53:55 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const colors = ["red", "green", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+    // get random number between 0 and color array length
+    const randomNumber = getRamdomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRamdomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
